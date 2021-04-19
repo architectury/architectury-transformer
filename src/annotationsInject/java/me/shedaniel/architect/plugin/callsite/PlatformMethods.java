92bfd382992022f804120afc0cc856a064bc49c0
shedaniel <daniel@shedaniel.me>
2021-04-19 14:11:37 +0800

    Tries to fallback onto fabric on quilt.

--- src/annotationsInject/java/me/shedaniel/architect/plugin/callsite/PlatformMethods.java
+++ src/annotationsInject/java/me/shedaniel/architect/plugin/callsite/PlatformMethods.java
@@ -30,58 +30,71 @@ import java.util.List;
 import java.util.Map;
 
 public class PlatformMethods {
-    public static CallSite platform(MethodHandles.Lookup lookup, String name, MethodType type) {
+    public static CallSite platform(MethodHandles.Lookup lookup, String methodName, MethodType methodType) {
+        String modLoader = getModLoader();
+        try {
+            return forPlatform(lookup, methodName, methodType, modLoader);
+        } catch (AssertionError error) {
+            if (modLoader.equals("quilt")) {
+                try {
+                    return forPlatform(lookup, methodName, methodType, "fabric");
+                } catch (AssertionError ignored) {
+                    throw error;
+                }
+            } else {
+                throw error;
+            }
+        }
+    }
+    
+    private static CallSite forPlatform(MethodHandles.Lookup lookup, String methodName, MethodType methodType, String platform) {
         Class<?> lookupClass = lookup.lookupClass();
         String lookupType = lookupClass.getName().replace("$", "") + "Impl";
-
-        String platformExpectedClass = lookupType.substring(0, lookupType.lastIndexOf('.')) + "." + getModLoader() + "." +
-                lookupType.substring(lookupType.lastIndexOf('.') + 1);
+        
+        String platformExpectedClass = lookupType.substring(0, lookupType.lastIndexOf('.')) + "." + platform + "." +
+                                       lookupType.substring(lookupType.lastIndexOf('.') + 1);
         Class<?> newClass;
         try {
             newClass = Class.forName(platformExpectedClass, false, lookupClass.getClassLoader());
         } catch (ClassNotFoundException exception) {
-            throw new AssertionError(lookupClass.getName() + "#" + name + " expected platform implementation in " + platformExpectedClass +
-                    "#" + name + ", but the class doesn't exist!", exception);
+            throw new AssertionError(lookupClass.getName() + "#" + methodName + " expected platform implementation in " + platformExpectedClass +
+                                     "#" + methodName + ", but the class doesn't exist!", exception);
         }
         MethodHandle platformMethod;
         try {
-            platformMethod = lookup.findStatic(newClass, name, type);
+            platformMethod = lookup.findStatic(newClass, methodName, methodType);
         } catch (NoSuchMethodException exception) {
-            throw new AssertionError(lookupClass.getName() + "#" + name + " expected platform implementation in " + platformExpectedClass +
-                    "#" + name + ", but the method doesn't exist!", exception);
+            throw new AssertionError(lookupClass.getName() + "#" + methodName + " expected platform implementation in " + platformExpectedClass +
+                                     "#" + methodName + ", but the method doesn't exist!", exception);
         } catch (IllegalAccessException exception) {
-            throw new AssertionError(lookupClass.getName() + "#" + name + " expected platform implementation in " + platformExpectedClass +
-                    "#" + name + ", but the method's modifier doesn't match the access requirements!", exception);
+            throw new AssertionError(lookupClass.getName() + "#" + methodName + " expected platform implementation in " + platformExpectedClass +
+                                     "#" + methodName + ", but the method's modifier doesn't match the access requirements!", exception);
         }
         return new ConstantCallSite(platformMethod);
     }
-
+    
     private static String modLoader = null;
-
+    
     public static String getModLoader() {
         if (modLoader == null) {
-            try {
-                modLoader = (String) Class.forName("me.shedaniel.architectury.platform.Platform").getDeclaredMethod("getModLoader").invoke(null);
-            } catch (Throwable ignored) {
-                List<String> loader = new ArrayList<>();
-                HashMap<String, String> MOD_LOADERS = new HashMap<>();
-                MOD_LOADERS.put("net.fabricmc.loader.FabricLoader", "fabric");
-                MOD_LOADERS.put("net.minecraftforge.fml.common.Mod", "forge");
-                MOD_LOADERS.put("org.quiltmc.loader.impl.QuiltLoaderImpl", "fabric");
-                for (Map.Entry<String, String> entry : MOD_LOADERS.entrySet()) {
-                    try {
-                        PlatformMethods.class.getClassLoader().loadClass(entry.getKey());
-                        loader.add(entry.getValue());
-                        break;
-                    } catch (ClassNotFoundException ignored1) {
-                    }
+            List<String> loader = new ArrayList<>();
+            HashMap<String, String> MOD_LOADERS = new HashMap<>();
+            MOD_LOADERS.put("net.fabricmc.loader.FabricLoader", "fabric");
+            MOD_LOADERS.put("net.minecraftforge.fml.common.Mod", "forge");
+            MOD_LOADERS.put("org.quiltmc.loader.impl.QuiltLoaderImpl", "quilt");
+            for (Map.Entry<String, String> entry : MOD_LOADERS.entrySet()) {
+                try {
+                    PlatformMethods.class.getClassLoader().loadClass(entry.getKey());
+                    loader.add(entry.getValue());
+                    break;
+                } catch (ClassNotFoundException ignored1) {
                 }
-                if (loader.isEmpty())
-                    throw new IllegalStateException("No detected mod loader!");
-                if (loader.size() >= 2)
-                    System.err.println("Detected multiple mod loaders! Something is wrong on the classpath! " + String.join(", ", loader));
-                modLoader = loader.get(0);
             }
+            if (loader.isEmpty())
+                throw new IllegalStateException("No detected mod loader!");
+            if (loader.size() >= 2)
+                System.err.println("Detected multiple mod loaders! Something is wrong on the classpath! " + String.join(", ", loader));
+            modLoader = loader.get(0);
         }
         return modLoader;
     }
